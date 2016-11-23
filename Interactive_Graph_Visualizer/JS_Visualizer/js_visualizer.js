ea3348dc1bdc38767fd3fdf274d207189e83055e
LNLD <ekrixis72@gmail.com>
2016-11-24 03:09:56 +0900

    ズーム実装．静的のみ．ノードをドラッグすると彼方に消える．

--- Interactive_Graph_Visualizer/JS_Visualizer/js_visualizer.js
+++ Interactive_Graph_Visualizer/JS_Visualizer/js_visualizer.js
@@ -1,35 +1,32 @@
-﻿var w = 400;
-var h = 400;
-var svg = d3.select("body").append("svg").attr({ width: w, height: h });
+﻿var w = 1000;
+var h = 1000;
+var v_x = 500
+var v_y = 500
+var v_default_width = v_width = 1000
+var v_default_height = v_height = 1000
 
 var nodes;
 var links;
-d3.json("graph.json", function (graph) {
+d3.json("symple_graph.json", function (graph) {
     //console.log(graph);   
     nodes = graph.nodes;
     links = graph.links;
     draw(graph);
 });
 
-var zoom = d3.behavior.zoom()
-    .translate([0, 0])
-    .scale(1)
-    .scaleExtent([1, 8])
-    .on("zoom", zoomed);
-
-//var g = svg.append("g");
-
-svg
-    .call(zoom) // delete this line to disable free zooming
-    .call(zoom.event);
-
-function zoomed() 
-{
-  svg.style("stroke-width", 1.5 / d3.event.scale + "px");
-  svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-}
-
 function draw(graph) {
+    var svg = d3.select("body")
+        .append("svg")
+        .attr({ width: w, height: h })
+        //.attr("viewBox", "" + v_x + " " + v_y + " " + v_width + " " + v_height);
+
+    var g = svg.append('g');
+    g.append("rect")//rectをグループ内に入れないとズームが効かない?
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("fill", "gray")
+        .attr("opacity",0.3);
+
     var force = d3.layout.force()
 				 .nodes(nodes)
 				 .links(links)
@@ -44,10 +41,21 @@ function draw(graph) {
 				 .alpha(0.1)
 				 .start();
 
+    var zoom = d3.behavior.zoom()
+        //.translate([0, 0])
+        //.scale(1)
+        //.scaleExtent([1, 8])
+        .scaleExtent([0.1, 10])
+        //.size([w,h])
+        .on("zoom", zoomed);
+
+    g.call(zoom); // delete this line to disable free zooming
+        //.call(zoom.event);
+
     force.start(); //force レイアウトの計算を開始
     for (var i = 500; i > 0; --i) force.tick(); //ワンステップ進める
 
-    var link = svg.selectAll("line")
+    var link = g.selectAll("line")
                      .data(links)
                      .enter()
                      .append("line")
@@ -55,16 +63,18 @@ function draw(graph) {
                          stroke: "#ccc",
                          "stroke-width": 1
                      })
-    .attr({
-        "x1": function (d) { return d.source.x },
-        "y1": function (d) { return d.source.y },
-        "x2": function (d) { return d.target.x },
-        "y2": function (d) { return d.target.y },
-    });
-    var node = svg.selectAll("circle")
+                    .attr("transform", function(d) { return "translate(" + d + ")"; })
+                    .attr({
+                        "x1": function (d) { return d.source.x },
+                        "y1": function (d) { return d.source.y },
+                        "x2": function (d) { return d.target.x },
+                        "y2": function (d) { return d.target.y },
+                    });
+    var node = g.selectAll("circle")
 				  .data(nodes)
 				  .enter()
 				  .append("circle")
+                    .attr("transform", function(d) { return "translate(" + d + ")"; })
 				  .attr({
 				      r: 20,
 				      opacity: 0.5
@@ -79,62 +89,33 @@ function draw(graph) {
                 })
 				  .call(force.drag);
 
-    force.stop(); //force レイアウトの計算を終了
+    //force.stop(); //force レイアウトの計算を終了
 
-    //zoomFit()
+    //force.on("tick", function() {
+    //    link.attr({x1: function(d) { return d.source.x; },
+    //        y1: function(d) { return d.source.y; },
+    //        x2: function(d) { return d.target.x; },
+    //        y2: function(d) { return d.target.y; }});
+    //    node.attr({cx: function(d) { return d.x; },
+    //        cy: function(d) { return d.y; }});
+    //});
 
-    force.on("tick", function() {
-      link.attr({x1: function(d) { return d.source.x; },
-                 y1: function(d) { return d.source.y; },
-                 x2: function(d) { return d.target.x; },
-                 y2: function(d) { return d.target.y; }});
-      node.attr({cx: function(d) { return d.x; },
-                 cy: function(d) { return d.y; }});
-    });
 
-    //// zoomビヘイビアの設定
-    //var zoom = d3.behavior.zoom()
-    //  .scaleExtent([0.1, 10])
-    //  .on("zoom", function(){
-    //    container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-    //  });
-     
-    //// SVG を zoom ビヘイビアに渡す
-    //var svg = d3.select("#MyGraph")
-    //  .attr("width", w)
-    //  .attr("height", h)
-    //  .append("g")
-    //  .attr("transform", "translate(0,0)")
-    //  .call(zoom);
-     
-    //var container = svg.append("g");
-     
-    //// pointer-events = all でマウスイベントをSVGの描画範囲全体で拾うよう設定
-    //var rect = svg.append("rect")
-    //  .attr("width", w)
-    //  .attr("height", h)
-    //  .style("fill", "none")
-    //  .style("pointer-events", "all");
+    function zoomed() 
+    {
+      //svg.style("stroke-width", 1.5 / d3.event.scale + "px");
+        g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+        var befere_vbox_width, before_vbox_height, d_x, d_y;
 
-    //function zoomFit(transitionDuration) {
-    //    var bounds = root.node().getBBox();
-    //    var parent = root.node().parentElement;
-    //    var fullWidth = parent.clientWidth || parent.parentNode.clientWidth,
-    //        fullHeight = parent.clientHeight || parent.parentNode.clientHeight;
-    //    var width = bounds.width,
-    //        height = bounds.height;
-    //    var midX = bounds.x + width / 2,
-    //        midY = bounds.y + height / 2;
-    //    if (width == 0 || height == 0) return; // nothing to fit
-    //    var scale = 0.85 / Math.max(width / fullWidth, height / fullHeight);
-    //    var translate = [fullWidth / 2 - scale * midX, fullHeight / 2 - scale * midY];
-
-    //    console.trace("zoomFit", translate, scale);
-
-    //    root
-    //        .transition()
-    //        .duration(transitionDuration || 0) // milliseconds
-    //        .call(zoom.translate(translate).scale(scale).event);
-    //}
+        //befere_v_width = v_width
+        //  before_v_height = v_height
+        //  v_width = v_default_width * d3.event.scale
+        //  v_height = v_default_height * d3.event.scale
+        //  d_x = (befere_v_width - v_width) / 2
+        //  d_y = (before_v_height - v_height) / 2
+        //  v_x += d_x
+        //  v_y += d_y
+        //  return svg.attr("viewBox", "" + v_x + " " + v_y + " " + v_width + " " + v_height);
+    }
 }
 
--- Interactive_Graph_Visualizer/JS_Visualizer/sample.js
+++ Interactive_Graph_Visualizer/JS_Visualizer/sample.js
@@ -1,63 +1,114 @@
-﻿
-var width = 960,
-    height = 500,
-    k = 80;
+﻿var margin = {top: 20, right: 60, bottom: 30, left: 20},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
-    .domain([0, width])
+var parseDate = d3.time.format("%Y-%m-%d").parse,
+    formatDate = d3.time.format("%Y");
+
+var x = d3.time.scale()
     .range([0, width]);
 
 var y = d3.scale.linear()
-    .domain([0, height])
-    .range([0, height]);
+    .range([height, 0]);
 
-var random = d3.random.normal(0, width);
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickSize(-height, 0)
+    .tickPadding(6);
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("right")
+    .tickSize(-width)
+    .tickPadding(6);
+
+var area = d3.svg.area()
+    .interpolate("step-after")
+    .x(function(d) { return x(d.date); })
+    .y0(y(0))
+    .y1(function(d) { return y(d.value); });
+
+var line = d3.svg.line()
+    .interpolate("step-after")
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.value); });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var zoom = d3.behavior.zoom()
-    .x(x)
-    .y(y)
-    .size([width, height])
-    .on("zoom", zoomed);
+    .on("zoom", draw);
 
-var canvas = d3.select("body").append("canvas")
+var gradient = svg.append("defs").append("linearGradient")
+    .attr("id", "gradient")
+    .attr("x2", "0%")
+    .attr("y2", "100%");
+
+gradient.append("stop")
+    .attr("offset", "0%")
+    .attr("stop-color", "#fff")
+    .attr("stop-opacity", .5);
+
+gradient.append("stop")
+    .attr("offset", "100%")
+    .attr("stop-color", "#999")
+    .attr("stop-opacity", 1);
+
+svg.append("clipPath")
+    .attr("id", "clip")
+  .append("rect")
+    .attr("x", x(0))
+    .attr("y", y(1))
+    .attr("width", x(1) - x(0))
+    .attr("height", y(0) - y(1));
+
+svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + width + ",0)");
+
+svg.append("path")
+    .attr("class", "area")
+    .attr("clip-path", "url(#clip)")
+    .style("fill", "url(#gradient)");
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")");
+
+svg.append("path")
+    .attr("class", "line")
+    .attr("clip-path", "url(#clip)");
+
+svg.append("rect")
+    .attr("class", "pane")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .call(zoom);
 
-var context = canvas.node().getContext("2d");
+d3.csv("readme-flights.csv", function(error, data) {
+  if (error) throw error;
 
-context.fillStyle = "#ccc";
-context.strokeStyle = "#fff";
+  data.forEach(function(d) {
+    d.date = parseDate(d.date);
+    d.value = +d.value;
+  });
 
-canvas.call(zoom.event);
+  x.domain([new Date(1999, 0, 1), new Date(2003, 0, 0)]);
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+  zoom.x(x);
 
-jump();
+  svg.select("path.area").data([data]);
+  svg.select("path.line").data([data]);
+  draw();
+});
 
-function zoomed() {
-  context.fillRect(0, 0, width, height);
-  context.lineWidth = 4 * zoom.scale();
-
-  for (var d = x.domain(),
-      x0 = Math.floor(d[0] / k) * k,
-      x1 = Math.ceil(d[1] / k) * k; x0 <= x1; x0 += k) {
-    context.beginPath();
-    context.moveTo(x(x0), 0);
-    context.lineTo(x(x0), height);
-    context.stroke();
-  }
-
-  for (var d = y.domain(),
-      y0 = Math.floor(d[0] / k) * k,
-      y1 = Math.ceil(d[1] / k) * k; y0 <= y1; y0 += k) {
-    context.beginPath();
-    context.moveTo(0, y(y0));
-    context.lineTo(width, y(y0));
-    context.stroke();
-  }
-}
-
-function jump() {
-  canvas.transition()
-      .duration(5000)
-      .call(zoom.translate([random(), random()]).event)
-      .each("end", jump);
+function draw() {
+  svg.select("g.x.axis").call(xAxis);
+  svg.select("g.y.axis").call(yAxis);
+  svg.select("path.area").attr("d", area);
+  svg.select("path.line").attr("d", line);
 }
